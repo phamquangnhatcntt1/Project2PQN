d772c1ba88ad72dce9ab76b5264078654aba6a5d
Phạm Quang Nhất <phamquangnhatdz2105@gmail.com>
2024-11-11 13:57:49 +0700

    2004

--- Proj2-PhamQuangNhat-2210900115/Ictshop/Areas/Admin/Controllers/NguoidungsController.cs
+++ Proj2-PhamQuangNhat-2210900115/Ictshop/Areas/Admin/Controllers/NguoidungsController.cs
@@ -42,30 +42,30 @@ namespace Ictshop.Areas.Admin.Controllers
         }
 
         //// GET: Admin/Nguoidungs/Create
-        //public ActionResult Create()
-        //{
-        //    ViewBag.IDQuyen = new SelectList(db.PhanQuyens, "IDQuyen", "TenQuyen");
-        //    return View();
-        //}
+        public ActionResult Create()
+        {
+            ViewBag.IDQuyen = new SelectList(db.PhanQuyens, "IDQuyen", "TenQuyen");
+            return View();
+        }
 
         //// POST: Admin/Nguoidungs/Create
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "MaNguoiDung,Hoten,Email,Dienthoai,Matkhau,IDQuyen")] Nguoidung nguoidung)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Nguoidungs.Add(nguoidung);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaNguoiDung,Hoten,Email,Dienthoai,Matkhau,IDQuyen")] Nguoidung nguoidung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Nguoidungs.Add(nguoidung);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDQuyen = new SelectList(db.PhanQuyens, "IDQuyen", "TenQuyen", nguoidung.IDQuyen);
+            return View(nguoidung);
+        }
 
-        //    ViewBag.IDQuyen = new SelectList(db.PhanQuyens, "IDQuyen", "TenQuyen", nguoidung.IDQuyen);
-        //    return View(nguoidung);
-        //}
 
 
-            // Chỉnh sửa người dùng
+        // Chỉnh sửa người dùng
         // GET: Admin/Nguoidungs/Edit/5
         public ActionResult Edit(int? id)
         {
